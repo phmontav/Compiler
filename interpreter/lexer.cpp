cce3894131971c9e4b90a6bb15b9529a731b4762
srmacario <sergioreinier@gmail.com>
2023-08-21 20:27:57 -0300

    código do Aguiar passado para classe

--- interpreter/lexer.cpp
+++ interpreter/lexer.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <boost/preprocessor.hpp>
 #include <algorithm>
+#include <string>
+#include <unordered_map>
 #define X_DEFINE_ENUM_WITH_STRING_CONVERSIONS_TOSTRING_CASE(r, data, elem) \
     case elem:                                                             \
         return BOOST_PP_STRINGIZE(elem);
@@ -36,41 +39,128 @@ DEFINE_ENUM_WITH_STRING_CONVERSIONS(TokenType,
                                     //token desconhecido
                                     (UNKNOWN))
 
-class Token
-{
-public:
-    TokenType tokenType;
-    int value;
+
+typedef unsigned char u_byte;
+typedef struct {
+    u_byte type; // 0-char, 1-int, 2- string
+    union {
+        char cVal;
+        int nVal;
+        char * sVal;
+    } _;
+} t_const;
+
+class Token{
+    public:
+        TokenType tokenType;
+        int value;
 };
 
-class Lexer
-{
-public:
+class Lexer{
+    public:
 
-    // Definindo as constantes globais
+        // Defina a tabela de identificadores como um mapa de hash que associa cada nome a um número inteiro
+        unordered_map<string, int> names;
 
-    const static int MAX_LENGTH = 256; // tamanho máximo de um lexema
-    const static int KEYWORDS_SIZE = 18; // número de palavras reservadas
-    const string KEYWORDS[KEYWORDS_SIZE] = { // vetor de palavras reservadas ordenado alfabeticamente
-        "ARRAY", "BOOLEAN", "BREAK", "CHAR", "CONTINUE", "DO", "ELSE", "FALSE", 
-        "FUNCTION", "IF", "INTEGER", "OF", "STRING", "STRUCT", "TRUE", "TYPE", 
-        "VAR", "WHILE"
-    };
+        // Defina a tabela de constantes como um vetor que armazena as estruturas t_const
+        vector<t_const> constants;
 
-    string text;
-    vector<string> TokenTypeVector;
-    Lexer(string &_text) {
-        this->text = _text;
-        Lexer();
-    }
-    Lexer() {
-        for (int i = ARRAY; i <= UNKNOWN; i++)
-        {
-            TokenType e = static_cast<TokenType>(i);
-            TokenTypeVector.push_back(ToString(e));
+        // Definindo as constantes globais
+        const static int MAX_LENGTH = 256; // tamanho máximo de um lexema
+        const static int KEYWORDS_SIZE = 18; // número de palavras reservadas
+        const string KEYWORDS[KEYWORDS_SIZE] = { // vetor de palavras reservadas ordenado alfabeticamente
+            "ARRAY", "BOOLEAN", "BREAK", "CHAR", "CONTINUE", "DO", "ELSE", "FALSE", 
+            "FUNCTION", "IF", "INTEGER", "OF", "STRING", "STRUCT", "TRUE", "TYPE", 
+            "VAR", "WHILE"
+        };
+
+        // Definindo as variáveis globais
+        ifstream input; // arquivo de entrada
+        ofstream output; // arquivo de saída
+        char lexeme[MAX_LENGTH]; // buffer para armazenar o lexema atual
+        int line = 1; // contador de linhas
+        int column = 0; // contador de colunas
+
+        string text;
+        vector<string> TokenTypeVector;
+        Lexer(string &_text) {
+            this->text = _text;
+            Lexer();
         }
-        sort(TokenTypeVector.begin(), TokenTypeVector.end());
-    }
+        Lexer() {
+            for (int i = ARRAY; i <= UNKNOWN; i++)
+            {
+                TokenType e = static_cast<TokenType>(i);
+                TokenTypeVector.push_back(ToString(e));
+            }
+            sort(TokenTypeVector.begin(), TokenTypeVector.end());
+        }
+        // Função para buscar uma palavra na tabela de palavras reservadas
+        TokenType searchKeyWord(string name) {
+            // Busca binária no vetor de palavras reservadas
+            int low = 0;
+            int high = KEYWORDS_SIZE - 1;
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                int cmp = name.compare(KEYWORDS[mid]);
+                if (cmp == 0) {
+                    // Palavra encontrada, retorna o token correspondente
+                    return (TokenType) mid;
+                }
+                else if (cmp < 0) {
+                    // Palavra é menor que o elemento do meio, busca na metade inferior
+                    high = mid - 1;
+                }
+                else {
+                    // Palavra é maior que o elemento do meio, busca na metade superior
+                    low = mid + 1;
+                }
+            }
+            // Palavra não encontrada, retorna o token de identificador
+            
+            return ID;
+        }
+
+        // Função para buscar ou inserir um identificador na tabela de nomes
+        int searchName(string name) {
+            // Busca na tabela de nomes usando um algoritmo de hash
+            std::string key(name);
+            auto it = names.find(key);
+            if (it != names.end()) {
+                // Identificador encontrado, retorna o seu nome (token secundário)
+                return it->second;
+            }
+            else {
+                // Identificador não encontrado, insere na tabela com um novo nome
+                int newName = static_cast<int>(names.size());
+                names[key] = newName;
+                return newName;
+            }
+        }
+
+        // Defina uma função para inserir uma constante na tabela de constantes e retornar seu índice no vetor
+        int insertConst( t_const c ) {
+            // Insere a constante no final do vetor de constantes
+            constants.push_back(c);
+            // Retorna o índice da constante inserida (igual ao tamanho atual do vetor menos um)
+            return static_cast<int>(constants.size()) - 1;
+        }
+
+        // Defina uma função para ler o próximo caractere do arquivo de entrada e retorná-lo
+        char nextChar() {
+            // Declara uma variável para armazenar o caractere lido
+            char c;
+            // Lê o próximo caractere do arquivo de entrada
+            input.get(c);
+            // Retorna o caractere lido
+            return c;
+        }
+
+        // Defina uma função para retornar o último caractere lido para o arquivo de entrada
+        void backChar() {
+            // Volta uma posição no arquivo de entrada
+            input.unget();
+        }
 };
 
 int main()
