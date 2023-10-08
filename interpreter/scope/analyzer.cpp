5e13ab85a49b9e412001892ef8a3d011a11d4992
phmontav <phmontav@gmail.com>
2023-10-08 16:59:31 -0300

    analyzer

--- interpreter/scope/analyzer.cpp
+++ interpreter/scope/analyzer.cpp
@@ -9,7 +9,6 @@ public:
     vector<Node *> SymbolTable;     // primeiro da lista encadeada
     vector<Node *> SymbolTableLast; // ultimo da lista encadeada
     int currentLevel = -1;
-    variant<Var, Param, Field, Function, Array, Struct, Alias> variant;
     int newBlock()
     {
         SymbolTable.push_back(nullptr);
@@ -73,8 +72,48 @@ public:
         return node;
     }
 
-    boolean check_types(Node *t1, Node *t2)
+    bool check_types(Node *t1, Node *t2) // CheckTypes
     {
+        if (t1 == t2)
+            return true;
+        if (t1 == universal_node || t2 == universal_node)
+            return true;
+        if (t1->type == UNIVERSAL_ || t2->type == UNIVERSAL_)
+            return true;
+        if (t1->type == t2->type)
+        {
+            if (t1->type == ALIAS_TYPE_)
+            {
+                Alias t1_Alias = get<Alias>(t1->variant);
+                Alias t2_Alias = get<Alias>(t2->variant);
+                return check_types(t1_Alias.base_type, t2_Alias.base_type);
+            }
+            if (t1->type == ARRAY_TYPE_)
+            {
+                Array t1_array = get<Array>(t1->variant);
+                Array t2_array = get<Array>(t2->variant);
+                if (t1_array.num_elements == t2_array.num_elements)
+                {
+                    return check_types(t1_array.element_type, t2_array.element_type);
+                }
+            }
+            else if (t1->type == STRUCT_TYPE_)
+            {
+                Struct t1_struct = get<Struct>(t1->variant);
+                Struct t2_struct = get<Struct>(t2->variant);
+                Node *f1 = t1_struct.fields;
+                Node *f2 = t2_struct.fields;
+                while (f1 != nullptr && f2 != nullptr)
+                {
+                    Field f1_field = get<Field>(f1->variant);
+                    Field f2_field = get<Field>(f2->variant);
+                    if (!check_types(f1_field.type, f2_field.type))
+                        return false;
+                }
+                return (f1 == nullptr && f2 == nullptr);
+            }
+        }
+        return false;
     }
 };
 
